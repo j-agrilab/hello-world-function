e6fe0992bfcdce71119e63bf1d0a6168b5f96cd6
j-agrilab <jjacobson@agrilabtech.com>
2025-01-31 21:21:24 -0800

    added try catch

--- main.py
+++ main.py
@@ -6,27 +6,30 @@ logger = logging.Client().logger(__name__)
 def main(event, context):
     """Uploads a file to a bucket."""
     logger.log_text("starting hello world function", severity="INFO")
-    # The ID of your GCS bucket
-    bucket_name = "hmidata" 
+    try:
+        # The ID of your GCS bucket
+        bucket_name = "hmidata" 
 
-    # The ID of your GCS object
-    blob_name = "test.txt"
+        # The ID of your GCS object
+        blob_name = "test.txt"
 
-    # The path to your file to upload
-    # In this case, we're creating the content dynamically
-    content = "hello world"
+        # The path to your file to upload
+        # In this case, we're creating the content dynamically
+        content = "hello world"
 
-    # Create a client
-    storage_client = storage.Client()
+        # Create a client
+        storage_client = storage.Client()
 
-    # Create a new bucket object
-    bucket = storage_client.bucket(bucket_name)
+        # Create a new bucket object
+        bucket = storage_client.bucket(bucket_name)
 
-    # Create a new blob object and upload data
-    blob = bucket.blob(blob_name)
-    blob.upload_from_string(content)
+        # Create a new blob object and upload data
+        blob = bucket.blob(blob_name)
+        blob.upload_from_string(content)
 
-    logger.log_text(f"File {blob_name} uploaded to {bucket_name}.", severity="INFO")
+        logger.log_text(f"File {blob_name} uploaded to {bucket_name}.", severity="INFO")
+    except:
+        logger.log_text(f"Error running hello world.", severity="ERROR")
 
 if __name__ == '__main__':
     main(None, None)
